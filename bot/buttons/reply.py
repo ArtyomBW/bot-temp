c30b5b2bf5c4c881fd1c29dd41c656e6107e5d75
Artyom <artyom.bw08@gmail.com>
2025-04-24 19:15:31 +0500

    commit

--- bot/buttons/reply.py
+++ bot/buttons/reply.py
@@ -7,3 +7,5 @@ def build_reply_button(texts, size, one_time_keyboard=False):
     rkb.add(*[KeyboardButton(text = text) for text in texts])
     rkb.adjust(*size)
     return rkb.as_markup(resize_keyboard=True, one_time_keyboard=one_time_keyboard)
+
+
--- bot/handler/main_handler.py
+++ bot/handler/main_handler.py
@@ -10,23 +10,23 @@ from bot.dispetcher import router
 from bot.states import SectorState
 
 
-@router.message(SectorState.language, F.text == __("⬅️  Back"))
-@router.message(SectorState.product_menu, F.text == __("⬅️ Back"))
+@router.message(SectorState.language, F.text == __("⬅️ Orqaga"))
+@router.message(SectorState.product_menu, F.text == __("⬅️ Orqaga"))
 @router.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
-    texts = [_("🎥 Movies Section"),
-             _("📞 Contact center"),
-             _("🇺🇿🇬🇧🇷🇺 Language")]
+    texts = [_("🍽 Restoran menyusi"),
+             _("📞 Biz bilan aloqa"),
+             _("🇺🇿🇬🇧 Til tanlash")]
     markup = build_reply_button(texts, (2, 1))
-    await message.answer(_(f"🏠 Main menu: \nBW"), reply_markup=markup)
+    await message.answer(_(f"🏠 Asosiy menyu"), reply_markup=markup)
+# 🍽☎️🥗🍕🍜⬅️
 
-
-@router.message(F.text == __("🇺🇿🇬🇧🇷🇺 Language"))
+@router.message(F.text == __("🇺🇿🇬🇧 Til tanlash"))
 async def language_handler(message: Message, state: FSMContext):
-    texts = [_("🇺🇿 Uzbek"), _("🇬🇧 English"), _("🇷🇺 Russia"), _("⬅️  Back")]
+    texts = [_("🇺🇿 Uzbek"), _("🇬🇧 English"),_("⬅️ Orqaga")]
     markup = build_reply_button(texts, (3, 1))
     await state.set_state(SectorState.language)
-    await message.answer(_(f"Choose language: \nBW"), reply_markup=markup)
+    await message.answer(_(f"🇺🇿🇬🇧 Tilni tanglang"), reply_markup=markup)
 
 
 @router.message(SectorState.language)
@@ -34,26 +34,22 @@ async def language_handler(message: Message, state: FSMContext, i18n):
     map_lang = {
         "🇺🇿 Uzbek": "uz",
         "🇬🇧 English": "en",
-        "🇷🇺 Russia": "ru",
     }
-
     code = map_lang.get(message.text)
     await state.update_data({"locale": code})
     i18n.current_locale = code
-    # await state.update_data({"locale": code})
-    texts = [_("🎥 Movies Section"), _("📞 Contact center"), _("🇺🇿🇬🇧🇷🇺 Language")]
+    texts = [_("🍽 Restoran menyusi"), _("📞 Biz bilan aloqa"), _("🇺🇿🇬🇧🇷🇺 Language")]
     markup = build_reply_button(texts, (2, 1))
-    await message.answer(_("🏠 Main menu : \nBW"), reply_markup=markup)
+    await message.answer(_("🏠 Asosiy menyu"), reply_markup=markup)
     await state.clear()
 
-
 @router.message(SectorState.movie_menu, F.text == __("⬅️ Back"))
-@router.message(F.text == __("🎥 Movies Section"))
+@router.message(F.text == __("🍽 Restoran menyusi"))
 async def movies_handler(message: Message, state: FSMContext):
-    texts = [_("🎭 Drama"),
-             _("😂 Comedy"),
-             _("🎬 Action"),
-             _("⬅️ Back")]
+    texts = [_("🍽 Restoran menyusi"),
+             _("🥗 Salatlar"),
+             _("🍕 Fas-fud"),
+             _("⬅️ Orqaga")]
     markup = build_reply_button(texts, (3, 1))
     await state.set_state(SectorState.product_menu)
-    await message.answer(_(f"🎥 Movies section: \nBW"), reply_markup=markup)
+    await message.answer(_(f"🍽 Restoran menyusi"), reply_markup=markup)
